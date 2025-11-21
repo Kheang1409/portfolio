518f61f50be0c9cf866660be0271f736bdffcde9
Kheang1409 <40851151+Kheang1409@users.noreply.github.com>
2025-11-20 23:03:16 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  useDeploymentId: true,
   output: "standalone",
+  useDeploymentIdServerActions: true,
 };
 
 module.exports = nextConfig;
